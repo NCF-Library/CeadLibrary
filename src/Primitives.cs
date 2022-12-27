646d5a3717f9334b065d174b874ec3bc7225646e
ArchLeaders <archparliament@gmail.com>
2022-12-27 00:51:05 -0800

    Impl primitive enums

---
+++ src/Primitives.cs
@@ -0,0 +1,17 @@
+﻿namespace NCF.BinaryExtensions
+{
+    public enum Endian : ushort
+    {
+        Big = 0xFFFE, Little = 0xFEFF
+    }
+
+    public enum BoolType : int
+    {
+        Byte = 1, Word = 2, DWord = 4, QWord = 8
+    }
+
+    public enum StringType
+    {
+        Int16CharCount, Int32CharCount, ZeroTerminated, Raw
+    }
+}
